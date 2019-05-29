26ff925dda1af3e51aa5816e7abaf411b5b12564
006 <006@006-PC>
2019-05-29 14:59:09 +0800

    add

--- spring-partybuild/spring-partybuild-service/src/main/java/com/cn/wisdom/base/service/impl/BaseOrganizationServiceImpl.java
+++ spring-partybuild/spring-partybuild-service/src/main/java/com/cn/wisdom/base/service/impl/BaseOrganizationServiceImpl.java
@@ -1,17 +1,24 @@
 package com.cn.wisdom.base.service.impl;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import com.cn.wisdom.base.dao.BaseDao;
 import com.cn.wisdom.base.model.BaseOrganization;
 import com.cn.wisdom.base.service.BaseOrganizationService;
 import com.cn.wisdom.infomation.dao.BaseOrganizationDao;
 @Service
 public class BaseOrganizationServiceImpl extends BaseServiceImpl<BaseOrganization,java.lang.String> implements BaseOrganizationService{
+
+	
+	@Resource
+	private BaseOrganizationDao baseOrganizationDao;
+	
+	@Override
+	public int delBatchByPrimary(String id) {
+		
+		return baseOrganizationDao.delBatchByPrimary(id);
+	}
 
 	
 	/*
