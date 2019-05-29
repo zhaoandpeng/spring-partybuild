1fd5bb9ba77671c32252079673fad47992b73222
006 <006@006-PC>
2019-05-29 14:58:32 +0800

    add

--- spring-partybuild/spring-partybuild-service/src/main/java/com/cn/wisdom/base/service/BaseOrganizationService.java
+++ spring-partybuild/spring-partybuild-service/src/main/java/com/cn/wisdom/base/service/BaseOrganizationService.java
@@ -1,11 +1,9 @@
 package com.cn.wisdom.base.service;
 
-import java.util.List;
-
 import com.cn.wisdom.base.model.BaseOrganization;
 
 public interface BaseOrganizationService extends BaseService<BaseOrganization, java.lang.String>{
 
-	/* void deleteByIds(List<String> ids); */
+	int delBatchByPrimary(String id);
 
 }
