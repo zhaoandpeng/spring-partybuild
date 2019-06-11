a6803d0d7d30445e1f554663c82e603d952576ed
006 <006@006-PC>
2019-06-11 16:59:40 +0800

    add

---
+++ spring-partybuild/spring-partybuild-utils/src/main/java/com/cn/wisdom/utils/Column.java
@@ -0,0 +1,14 @@
+package com.cn.wisdom.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface Column {
+
+	public String name(); //数据库字段名
+}
